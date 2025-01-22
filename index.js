e69f2e5bf9555648d49af45d40c165d8fd228d86
KR-ISCOD3 <krisjenxz@example.com>
2025-01-22 17:46:22 +0700

    cors-update

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router("data.json");
 const middleware = jsonServer.defaults();
+const cors = require('cors');
 
 server.use(router);
 server.use(middleware)
+server.use(cors()); // npm i cors
 
 const PORT = 3000;
 server.listen(PORT,()=>{
