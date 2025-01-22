b5b357e3d1c2ef9299b9558836d2b5ce595c340b
KR-ISCOD3 <krisjenxz@example.com>
2025-01-22 17:49:56 +0700

    third-commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router("data.json");
 const middleware = jsonServer.defaults();
-const cors = require('cors');
+const {cors} = require('cors');
 
 server.use(router);
 server.use(middleware)
